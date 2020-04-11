b28adfb52c7778f40d1fc331b90841e24cb3ab4e
Jesse Ssempijja <jessessempijja@gmail.com>
2020-04-11 10:10:52 +0300

    Pushing challenge one 16

--- src/estimator.js
+++ src/estimator.js
@@ -1,4 +1,39 @@
-import * as utils from './utils';
+// import * as utils from './utils';
+
+const formatWeeksToDays = (duration) => duration * 7;
+
+const formatMonthsToWeeks = (duration) => duration * 4;
+
+const formatMonthsToDays = (duration) => formatWeeksToDays(formatMonthsToWeeks(duration));
+
+const formatDuration = (data) => {
+  const { periodType } = data;
+  const period = data.timeToElapse;
+
+  switch (periodType) {
+    case 'days': {
+      return period;
+    }
+
+    case 'weeks': {
+      return formatWeeksToDays(period);
+    }
+
+    case 'months': {
+      const estimatedExtraDays = 2 * period;
+      return formatMonthsToDays(period) + estimatedExtraDays;
+    }
+
+    default: {
+      return period;
+    }
+  }
+};
+
+const CalculateFactor = (data) => {
+  const days = formatDuration(data);
+  return days / 3;
+};
 
 const covid19ImpactEstimator = (data) => {
   const { reportedCases } = data;
@@ -9,11 +44,11 @@ const covid19ImpactEstimator = (data) => {
     data,
     impact: {
       currentlyInfected: estimatedImpct,
-      infectionsByRequestedTime: Math.ceil(estimatedImpct * 2 ** utils.CalculateFactor(data))
+      infectionsByRequestedTime: estimatedImpct * 2 ** CalculateFactor(data)
     },
     severeImpact: {
       currentlyInfected: estimatedSevereImpct,
-      infectionsByRequestedTime: Math.ceil(estimatedSevereImpct * 2 ** utils.CalculateFactor(data))
+      infectionsByRequestedTime: estimatedSevereImpct * 2 ** CalculateFactor(data)
     }
 
   };
@@ -27,13 +62,15 @@ const covid19ImpactEstimator = (data) => {
     avgDailyIncomePopulation: 0.71
     },
     periodType: "days",
-    timeToElapse: 28,
+    timeToElapse: 31,
     reportedCases: 674,
     population: 66622705,
     totalHospitalBeds: 1380614
     }
+
+console.log('formatDuration ',formatDuration(data));
 console.log('******************');
-console.log(CalculateFactor(data));
+console.log('CalculateFactor ',CalculateFactor(data));
 console.log('******************');
 console.log(covid19ImpactEstimator(data));
 console.log('******************'); */
