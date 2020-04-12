bcfdf3b76e0f99a267d1c143308908f5d8a8f805
Jesse Ssempijja <jessessempijja@gmail.com>
2020-04-12 22:41:34 +0300

    Pushing challenge four

---
+++ public/js/formatData.js
@@ -0,0 +1,22 @@
+//import * as estimator from '../../src/estimator';
+const estimator = require('../../src/estimator');
+
+const grabData = () => {
+    console.log(`I've been clicked!`);
+    // const population
+    // const timeToElapse
+    // const reportedCases
+    // const totalHospitalBeds
+    // const periodType
+
+    // const input = {
+    //     population,
+    //     timeToElapse,
+    //     reportedCases,
+    //     totalHospitalBeds,
+    //     periodType
+    // };
+
+    // const output = estimator.default(input);
+};
+
